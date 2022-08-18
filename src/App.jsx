cda0556b0d201243cb4c188730a814bffcc4e379
qiyue365 <qiyue365@gmail.com>
2022-08-18 10:32:53 -0400

    交互

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React, { Fragment, useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const [msg, setMsg] = useState("Hello world!");
+  const defaultMsg = "Hello world!";
+  const [msg, setMsg] = useState(defaultMsg);
   return (
     <Fragment>
-      <div className="w-1/2 mx-auto mt-5">
+      <div className="w-full mx-auto mt-5 bg-white p-3 border rounded">
         <h1 className="text-3xl font-bold underline mb-3">{msg}</h1>
         <button
           className="px-4 py-2 bg-blue-400 hover:bg-blue-500 hover:outline-blue-600 text-gray-100"
-          onClick={() => setMsg("你好，世界")}
+          onClick={() =>
+            setMsg((state) =>
+              state === defaultMsg ? "你好，世界" : defaultMsg
+            )
+          }
         >
           点我
         </button>
